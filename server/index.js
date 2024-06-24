576646cf51ee4d97107ed18fbde69ed12a434c29
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-06-24 20:25:59 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const MONGO_URI =
 	"mongodb+srv://wheazzyfinesse:mydb1@mydb.5opdskt.mongodb.net/?retryWrites=true&w=majority&appName=mydb";
 
 app.use("/financial-records", financialrecordsroute);
+app.get("/", (req,res)=>{
+	res.send("welcome")
+}
 
 mongoose
 	.connect(MONGO_URI)
